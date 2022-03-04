f37d357e97abc63ce21037943ba6dff9eb23de4b
AbrhamAsfaw <abrhamasfaw2018@gmail.com>
2022-03-04 18:23:13 +0300

    Added Configuration in Startup class

---
+++ PLAY.CATALOG/src/Play.Catalog.Service/Settings/ServiceSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Catalog.Service.Settings
+{
+    public class ServiceSettings
+    {
+        public string ServiceName { get; init; }
+    }
+}
